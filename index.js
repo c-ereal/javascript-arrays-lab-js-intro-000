1b8bae29d9cdd13aa27b7326b9423d80c94e9b1c
bere abreo <berenicej2013@gmail.com>
2021-04-15 06:31:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
 
-function destructivelyAppendKitten(name) {
-  kittens = [...kittens, name];
-  return kittens;
+function destructivelyAppendKitten() {
+  return kittens.push();
+  
+}
+
+function destructivelyPrependKitten(name) {
+  
 }
