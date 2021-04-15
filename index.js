003bc1058e38a5da97ee798cfeff3b0c39ec514e
bere abreo <berenicej2013@gmail.com>
2021-04-15 06:28:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 // Add your functions and code here
 
 function destructivelyAppendKitten(name) {
-  moreKittens = [...kittens, name];
-  return moreKittens;
+  kittens = [...kittens, name];
+  return kittens;
 }
